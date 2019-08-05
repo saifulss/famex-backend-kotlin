e82fb9b59991422024a1606c218fb45602c14eb8
Saiful Shahril Saini <saifulss85@gmail.com>
2019-08-05 21:10:56 +0800

    Add integration test

--- src/main/kotlin/com/example/demo/HtmlController.kt
+++ src/main/kotlin/com/example/demo/HtmlController.kt
@@ -10,7 +10,7 @@ class HtmlController {
 
     @GetMapping("/")
     fun blog(model: Model): String {
-        model["title"] = "Blog-blog"
+        model["title"] = "Blog"
         return "blog"
     }
 
