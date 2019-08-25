08870d9e294eb07ca818ab292d1c7323fae9a8dd
saifulss <saifulss85@gmail.com>
2019-08-25 19:55:28 +0800

    Add JaCoCo plugin so we can run code coverage reports.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
+    jacoco
     kotlin("plugin.jpa") version "1.2.71"
     id("org.springframework.boot") version "2.1.6.RELEASE"
     id("io.spring.dependency-management") version "1.0.7.RELEASE"
