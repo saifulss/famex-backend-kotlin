87dfba3f4a0b2ae2f8b767fc0d8d2125c32c7f7d
saifulss <saifulss85@gmail.com>
2019-08-25 11:29:00 +0800

    Make email column unique.

--- src/main/resources/db/migration/V1.0.0__create_tables.sql
+++ src/main/resources/db/migration/V1.0.0__create_tables.sql
@@ -11,9 +11,9 @@ CREATE TABLE expense_claims
 
 CREATE TABLE users
 (
-  id INT(11) NOT NULL AUTO_INCREMENT,
-  display_name VARCHAR(255) NOT NULL,
-  email VARCHAR(255) NOT NULL,
-  password VARCHAR(255) NOT NULL,
-  PRIMARY KEY (id)
+    id           INT(11)      NOT NULL AUTO_INCREMENT,
+    display_name VARCHAR(255) NOT NULL,
+    email        VARCHAR(255) NOT NULL UNIQUE,
+    password     VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
 );
